b6ef1c664a9a2bc86648db4acd60d6519433a3de
rinku176 <rinkubijoy2000@gmail.com>
2024-06-05 17:46:10 +0100

    Basic | 17:46 | 5.6.24

---
+++ Assets/Scripts/TileSystem/Tilesystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilesystem : MonoBehaviour
+{
+    [SerializeField] Tilesystem TileSystem;
+    public enum SoilType
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+
+    public enum WaterType
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+
+    public bool energy = TileSystem.
+}

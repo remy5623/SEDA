dff0d1a3c8a0d95d1cca3aa9dc9bc8d1a7c14911
Remy Pijuan <guitarguru767@gmail.com>
2024-06-07 16:36:15 +0100

    Level Select Pauses Game Time | 16:34 | 07.06.24

--- Assets/Scripts/Menus and UI/LevelSelect.cs
+++ Assets/Scripts/Menus and UI/LevelSelect.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
+    private LevelSelect instance;
+
+    /** The level select menu is a singleton
+     *  There is only one level selection menu active at any given time
+     */
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    /** When the active instance of the level select menu is closed, game time will resume */
+    private void OnDestroy()
     {
-        
+        if (instance == this)
+        {
+            Time.timeScale = 1f;    // Unpause when the level selection menu is closed
+        }
     }
 }
